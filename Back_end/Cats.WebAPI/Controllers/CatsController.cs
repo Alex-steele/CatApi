5ba053cdd3707700329629fe0c13b94088a17331
Alex Steele <alex.steele@bud.co.uk>
2021-08-14 15:39:58 +0100

    Initial commit

---
+++ Back_end/Cats.WebAPI/Controllers/CatsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Cats.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatsController : ControllerBase
+    {
+        public CatsController()
+        {
+        }
+    }
+}
